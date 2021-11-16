a12a3bdb46a2bcdc053a84b1eba9f05284ccd675
Sara Sophia Masood <ssmasood@ncsu.edu>
2021-11-16 10:38:10 -0500

    Created UC19 test class

---
+++ iTrust2/src/test/java/edu/ncsu/csc/iTrust2/api/APIVaccinationCertificateTest.java
@@ -0,0 +1,42 @@
+package edu.ncsu.csc.iTrust2.api;
+
+// import static
+// org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static
+// org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+// import static
+// org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+// import static
+// org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+// import static
+// org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+// import javax.transaction.Transactional;
+//
+// import org.junit.Assert;
+// import org.junit.Before;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import
+// org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.http.MediaType;
+// import org.springframework.security.test.context.support.WithMockUser;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+// import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Test for API functionality for downloading a vaccination certificate
+ *
+ * @author Sara Sophia Masood
+ *
+ */
+// @RunWith ( SpringRunner.class )
+// @SpringBootTest
+// @AutoConfigureMockMvc
+public class APIVaccinationCertificateTest {
+
+}
