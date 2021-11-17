1505ab9917e926c9c63ce5b73d0ab63d37caaaf8
neworth <neworth@ncsu.edu>
2021-11-16 20:52:23 -0500

    Fully implemented pdf generation

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIVaccinationCertificateController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIVaccinationCertificateController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
+import edu.ncsu.csc.iTrust2.models.Patient;
 import edu.ncsu.csc.iTrust2.models.User;
 import edu.ncsu.csc.iTrust2.models.VaccinationVisit;
 import edu.ncsu.csc.iTrust2.models.enums.TransactionType;
@@ -21,6 +24,12 @@ import edu.ncsu.csc.iTrust2.services.UserService;
 import edu.ncsu.csc.iTrust2.services.VaccinationVisitService;
 import edu.ncsu.csc.iTrust2.utils.LoggerUtil;
 
+/**
+ * Class to implement UC19, the ability of a patient to download a vaccination
+ * certificate
+ *
+ * @author Nicole Worth
+ */
 @RestController
 public class APIVaccinationCertificateController extends APIController {
 
@@ -42,11 +51,16 @@ public class APIVaccinationCertificateController extends APIController {
     @Autowired
     private LoggerUtil              loggerUtil;
 
+    /**
+     * Method to download a vaccine certificate for a user
+     *
+     * @return the success/failure message as to whether a pdf is created
+     */
     @SuppressWarnings ( { "rawtypes", "unchecked" } )
     @GetMapping ( BASE_PATH + "/vaccinecertificate" )
     // TODO Do we need a PreAuthorize here?
     public ResponseEntity downloadCertificate () {
-        final User self = userService.findByName( LoggerUtil.currentUser() );
+        final Patient self = (Patient) userService.findByName( LoggerUtil.currentUser() );
         loggerUtil.log( TransactionType.VIEW_ALL_VACCINATION_VISITS, self );
         final List<VaccinationVisit> visits = visitService.findByPatient( self );
 
@@ -54,7 +68,44 @@ public class APIVaccinationCertificateController extends APIController {
         try {
             output = new FileOutputStream( new File( "Certificate.pdf" ) );
             final Document certificate = new Document();
+            PdfWriter.getInstance( certificate, output );
+            certificate.open();
+            certificate.addCreationDate();
+            certificate.addTitle( "Vaccination Certificate" );
+            certificate.addCreator( "iTrust Healthcare System" );
 
+            certificate.add( new Paragraph( "Patient: " + self.getFirstName() + " " + self.getLastName() ) );
+            boolean vaccinated = false;
+            for ( final VaccinationVisit visit : visits ) {
+                certificate.add( new Paragraph( "COVID-19 Vaccine: " + visit.getVaccines().getName() ) );
+                certificate.add( new Paragraph( "Date: " + visit.getDate().getDayOfMonth() + "/"
+                        + visit.getDate().getDayOfYear() + "/" + visit.getDate().getYear() ) );
+                // TODO how specific do we want to get with the time? It's
+                // currently in military format
+                certificate.add(
+                        new Paragraph( "Time: " + visit.getDate().getHour() + ":" + visit.getDate().getMinute() ) );
+                certificate.add( new Paragraph( "Staff Member: " + visit.getVaccinator().getUsername() ) );
+                int doses = 1;
+                if ( visit.getVaccines().getIfSecondDose() ) {
+                    doses = 2;
+                }
+                certificate.add( new Paragraph( "Dose: " + visit.getVisitNumber() + " of " + doses ) );
+
+                if ( doses == 1 && visits.size() == 1 ) {
+                    vaccinated = true;
+                }
+                else if ( doses == 2 && visits.size() == 2 ) {
+                    vaccinated = true;
+                }
+            }
+            if ( vaccinated ) {
+                certificate.add( new Paragraph( "Vaccination Status: Fully Vaccinated" ) );
+            }
+            else {
+                certificate.add( new Paragraph( "Vaccination Status: Not Fully Vaccinated" ) );
+            }
+
+            certificate.close();
             return new ResponseEntity( certificate, HttpStatus.OK );
         }
         catch ( final Exception ex ) {
