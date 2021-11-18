eb9262cff43cd5c99d761d3f7a1456858e622d12
Justin Takamiya <jjtakami@ncsu.edu>
2021-11-17 20:31:17 -0500

    Updated VaccinationVisitTest

--- iTrust2/src/test/java/edu/ncsu/csc/iTrust2/unit/VaccinationVisitTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/iTrust2/unit/VaccinationVisitTest.java
@@ -2,6 +2,7 @@ package edu.ncsu.csc.iTrust2.unit;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -17,14 +18,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.ncsu.csc.iTrust2.TestConfig;
 import edu.ncsu.csc.iTrust2.forms.UserForm;
+import edu.ncsu.csc.iTrust2.models.BasicHealthMetrics;
 import edu.ncsu.csc.iTrust2.models.Hospital;
+import edu.ncsu.csc.iTrust2.models.OfficeVisit;
 import edu.ncsu.csc.iTrust2.models.Patient;
 import edu.ncsu.csc.iTrust2.models.Personnel;
 import edu.ncsu.csc.iTrust2.models.User;
 import edu.ncsu.csc.iTrust2.models.VaccinationAppointmentRequest;
 import edu.ncsu.csc.iTrust2.models.VaccinationVisit;
 import edu.ncsu.csc.iTrust2.models.Vaccine;
+import edu.ncsu.csc.iTrust2.models.enums.AppointmentType;
+import edu.ncsu.csc.iTrust2.models.enums.HouseholdSmokingStatus;
 import edu.ncsu.csc.iTrust2.models.enums.Role;
+import edu.ncsu.csc.iTrust2.services.BasicHealthMetricsService;
 import edu.ncsu.csc.iTrust2.services.HospitalService;
 import edu.ncsu.csc.iTrust2.services.UserService;
 import edu.ncsu.csc.iTrust2.services.VaccinationVisitService;
@@ -33,10 +39,20 @@ import edu.ncsu.csc.iTrust2.services.VaccineService;
 @RunWith ( SpringRunner.class )
 @EnableAutoConfiguration
 @SpringBootTest ( classes = TestConfig.class )
+
+/**
+ * Tests Vaccination Visit.
+ * Based off of OfficeVisitTest since VaccinationVisit extends OfficeVisit.
+ * 
+ * @author Justin Takamiya (jjtakami@ncsu.edu)
+ */
 public class VaccinationVisitTest {
 
     @Autowired
     private VaccinationVisitService        vaccinationVisitService;
+    
+    @Autowired
+    private BasicHealthMetricsService basicHealthMetricsService;
 
     @Autowired
     private HospitalService           hospitalService;
@@ -46,16 +62,19 @@ public class VaccinationVisitTest {
    
     @Autowired
     private VaccineService            vaccineService;
+    
+    private User vaccinator;
 
     @Before
     public void setup () {
         vaccinationVisitService.deleteAll();
+        vaccineService.deleteAll();
 
         final User hcp = new Personnel( new UserForm( "hcp", "123456", Role.ROLE_HCP, 1 ) );
 
         final User alice = new Patient( new UserForm( "AliceThirteen", "123456", Role.ROLE_PATIENT, 1 ) );
         
-        final User vaccinator = new Personnel( new UserForm("vaccinator", "123456", Role.ROLE_VACCINATOR, 1));
+        vaccinator = new Personnel( new UserForm("vaccinator", "123456", Role.ROLE_VACCINATOR, 1));
         
         userService.saveAll( List.of( hcp, alice, vaccinator ) );
     }
@@ -63,8 +82,6 @@ public class VaccinationVisitTest {
     @Test
     @Transactional
     public void testVaccinationVisit () {
-    	
-    	vaccinationVisitService.deleteAll();
     	
         Assert.assertEquals( 0, vaccinationVisitService.count() );
 
@@ -79,32 +96,32 @@ public class VaccinationVisitTest {
         v.setIfSecondDose( true );
         v.setDaysBetween( 21 );
         v.setIfAvailable( true );
-        
         vaccineService.save(v);
-        
-        final VaccinationAppointmentRequest var = new VaccinationAppointmentRequest();
-        
-        var.setVaccineType(v);
 
         final VaccinationVisit visit = new VaccinationVisit();
-       
-        // Adding information specific to VaccinationVisit.
-        
-        // Setting the vaccinator
-        visit.setVaccinator( userService.findByName( "vaccinator" ));
-        // Setting the vaccination appointment request
-        visit.setAppointment(var);
-        // Setting the vaccine
+
+        final BasicHealthMetrics bhm = new BasicHealthMetrics();
+
+        bhm.setDiastolic( 150 );
+        bhm.setDiastolic( 100 );
+        bhm.setHcp( userService.findByName( "hcp" ) );
+        bhm.setPatient( userService.findByName( "AliceThirteen" ) );
+        bhm.setHdl( 75 );
+        bhm.setHeight( 75f );
+        bhm.setHouseSmokingStatus( HouseholdSmokingStatus.NONSMOKING );
+
+        basicHealthMetricsService.save( bhm );
+
+        visit.setBasicHealthMetrics( bhm );
+        visit.setType( AppointmentType.GENERAL_CHECKUP );
+        visit.setHospital( hosp );
+        visit.setPatient( userService.findByName( "AliceThirteen" ) );
+        visit.setHcp( userService.findByName( "AliceThirteen" ) );
+        visit.setDate( ZonedDateTime.now() );
         visit.setVaccines(v);
-                
-        assertEquals(var, visit.getAppointment());
+        visit.setVaccinator(userService.findByName("vaccinator"));
+        vaccinationVisitService.save( visit );
         
-        assertEquals(v, visit.getVaccines());
-        
-        User vaccinator = visit.getVaccinator();
-        
-        assertEquals(vaccinator, visit.getVaccinator());
-        
+        Assert.assertEquals( 1, vaccinationVisitService.count() );
     }
 }
-
